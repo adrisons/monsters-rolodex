3a66fdfd5f88c613eaf774cdd24d498e175e42df
Adrián Pereira Guerra <adrian.pereira@nomasystems.com>
2021-02-09 17:17:58 +0100

    feat: card component

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./card.styles.css";
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <h1 key={props.monster.id}>{props.monster.name}</h1>
+    </div>
+  );
+};
